2b8e284e6e6e929aae01e4a82d79054721795f1a
Brendan Burns <brendan.d.burns@gmail.com>
2016-03-25 05:40:05 +0100

    Add some more tests

--- metaparticle-file-storage.js
+++ metaparticle-file-storage.js
@@ -28,35 +28,34 @@
      */
     module.exports.load = function(scope) {
         var deferred = q.defer();
-	var path = scope + ".json";
-	// TODO: make this async too
-	var stats = null;
-	try {
-		stats = fs().statSync(path);
-	} catch (err) {
-	        if (err.code != 'ENOENT') {
-		  deferred.reject(err);
-		  return;
-		}
-	}
-	console.log(JSON.stringify(stats));
-        fs().readFile(path, function(err, data) {
+        var path = scope + ".json";
+        // TODO: make this async too
+        var stats = null;
+        try {
+            stats = fs().statSync(path);
+        } catch (err) {
+            if (err.code != 'ENOENT') {
+                deferred.reject(err);
+                return deferred.promise;
+            }
+        }
+        fs().readFile(path, 'utf-8', function(err, data) {
             if (err) {
-		if (err.code == 'ENOENT') {
-		  deferred.resolve({
-		    'data': {},
-		    'version': 'empty'
-		  });
-		} else {
-                  deferred.reject(err);
-		}
+                if (err.code == 'ENOENT') {
+                    deferred.resolve({
+                        'data': {},
+                        'version': 'empty'
+                    });
+                } else {
+                    deferred.reject(err);
+                }
             } else {
                 try {
                     var obj = JSON.parse(data);
                     deferred.resolve({
-			    'data': obj,
-		            'version': stats.mtime,
-	            });
+                        'data': obj,
+                        'version': stats.mtime,
+                    });
                 } catch (ex) {
                     deferred.reject(ex);
                 }
@@ -73,6 +72,32 @@
      */
     module.exports.store = function(scope, data) {
         var deferred = q.defer();
+        // TODO: make this async too                                                                                                  
+        var stats = null;
+	var empty = false;
+	var path = scope + '.json';
+        try {
+            stats = fs().statSync(path);
+        } catch (err) {
+            if (err.code != 'ENOENT') {
+                deferred.reject(err);
+                return deferred.promise;
+            } else {
+	        empty = true;
+	    }
+        }
+
+	if (empty) {
+		if (data.version != 'empty') {
+			deferred.resolve(false);	
+		}
+	} else {
+		if (data.version != stats.m_time) {
+			deferred.resolve(false);	
+		}
+	}
+
+
         var str = JSON.stringify(data.data);
         fs().writeFile(scope + ".json", str, function(err) {
             if (err) {
--- test/metaparticle-file-storage_test.js
+++ test/metaparticle-file-storage_test.js
@@ -7,27 +7,43 @@ describe('file-storage', function() {
     before(function(done) {
         this.timeout(10000);
         mp = require('../metaparticle-file-storage');
-	scope = 'scope-' + Math.floor(Math.random() * 10000);
+        scope = 'scope-' + Math.floor(Math.random() * 10000);
         done();
     });
 
-    after(function(done) {
+    afterEach(function(done) {
         require('fs').unlink(scope + '.json', function() {
-		done();
-	});
+            done();
+        });
     })
 
-    it('should return empty if no such scope', function(done){
-       var promise = mp.load('no-such-scope');
-       promise.then(function(data) {
-         test.object(data).is({});
-         done();
-       },
-       function(err) {
-	 // This should never be called.
-         test.bool(false).isTrue();
-	 done();
-       }).done();
+    it('should return empty if no such scope', function(done) {
+        var promise = mp.load('no-such-scope');
+        promise.then(function(data) {
+                test.object(data.data).is({});
+                test.string(data.version).is('empty');
+                done();
+            },
+            function(err) {
+                // This should never be called.
+                test.assert.fail(err);
+                done();
+            }).done();
+    });
+
+    it('should be able to write if it was empty', function(done) {
+        var promise = mp.store(scope, {
+            data: {},
+            version: 'empty'
+        });
+        promise.then(function(success) {
+                test.bool(success).isTrue();
+                done();
+            },
+            function(err) {
+                test.assert.fail(err);
+                done();
+            }).done();
     });
 
     it('should round trip successfully', function(done) {
@@ -36,13 +52,84 @@ describe('file-storage', function() {
             'baz': 'blah'
         };
 
-        var promise = mp.store(scope, obj);
+        var promise = mp.store(scope, {
+            'data': obj,
+            'version': 'empty'
+        });
 
         promise.then(function() {
             mp.load(scope).then(function(loaded) {
-            	test.object(loaded).is(obj);
-	    	done();
-	    }).done();
+                test.object(loaded.data).is(obj);
+                done();
+            }).done();
+        }).done();
+    });
+
+    it('should fail if there is a conflict non-exist', function(done) {
+        var obj = {
+            'foo': 'bar'
+        };
+        var promise = mp.store(scope, {
+            'data': obj,
+            'version': 'non-empty-version'
+        });
+
+        promise.then(function(success) {
+                test.bool(success).isFalse();
+                // TODO: test here that no file was written
+                done();
+            },
+            function(err) {
+                test.assert.fail(err);
+                done();
+            }).done();
+    });
+
+    it('should fail if there is a conflict exist', function(done) {
+        var obj = {
+            'foo': 'bar',
+            'baz': 'blah'
+        };
+
+        var data = {
+            'data': obj,
+            'version': 'empty'
+        };
+
+        var promise = mp.store(scope, data);
+
+        promise.then(function(success) {
+            test.bool(success).isTrue();
+            return mp.store(scope, data);
+        }).then(function(success) {
+            test.bool(success).isFalse()
+            done()
+        }).done();
+    });
+
+    it('should fail if there is a conflict different version', function(done) {
+        var obj = {
+            'foo': 'bar',
+            'baz': 'blah'
+        };
+
+        var data = {
+            'data': obj,
+            'version': 'empty'
+        };
+
+        var promise = mp.store(scope, data);
+
+        promise.then(function(success) {
+            test.bool(success).isTrue();
+            return mp.load(scope, data);
+        }).then(function(data) {
+	    data.version = 'no-such-version';
+	    data.data['foo'] = 'baz';
+	    return mp.store(scope, data);
+	}).then(function(success) {
+            test.bool(success).isFalse();
+            done()
         }).done();
     });
 });
