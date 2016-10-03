e966636efe7d9404490d8dfc0ce046a820bcf009
Brendan Burns <brendan.d.burns@gmail.com>
2016-10-02 22:39:52 -0700

    Add verbose logging to the client.

--- client.js
+++ client.js
@@ -1,8 +1,14 @@
 var jayson = require('jayson');
+var log = require('loglevel');
 
 // create a client
 var client = jayson.client.http({
   port: 3000
+});
+
+client.on('http request', (req) => {
+	log.debug("REQUEST:");
+	log.debug(req);
 });
 
 client.request(process.argv[2], [{'foo': 'bar'}], function(err, response) {
